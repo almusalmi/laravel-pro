f6d5a96d026f12341535aaf3748dee028c0df1e1
almusalmi <37040663+almusalmi@users.noreply.github.com>
2021-02-02 14:31:14 +0300

    =first commit

---
+++ app/Http/Controllers/Front/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function welcome(){
+        return 'Hello Laravel  && Vue';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/','welcome');
+
+Route::group(['namespace'=>'Front'],function() {
+    Route::get('/welcome','UserController@welcome');
+});
+//Route::get('/welcome','Front\UserController@welcome');
+//});
+
+Auth::routes(['verify'=>true]);
+
+Route::get('/home', 'HomeController@index');
+Route::get('/dashboard', function () {
+        return 'dashboard';
+});
